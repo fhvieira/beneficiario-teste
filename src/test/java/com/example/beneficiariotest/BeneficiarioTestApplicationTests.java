13c447fc2fff92a2a87803ec39db410feeb96e1d
fhvieira <fhvieira@hotmail.com>
2023-09-11 17:58:15 -0300

    configuracao cobertura testes

--- src/test/java/com/example/beneficiariotest/BeneficiarioTestApplicationTests.java
+++ src/test/java/com/example/beneficiariotest/BeneficiarioTestApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.beneficiariotest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BeneficiarioTestApplicationTests {
 
-//    @Test
+    @Test
     void contextLoads() {
+        String[] args = {};
+
+        assertDoesNotThrow(() -> BeneficiarioTestApplication.main(args));
     }
 
 }
